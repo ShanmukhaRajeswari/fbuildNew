ed8aaab67f64266322bc9b5b54c804596a6f830a
ShanmukhaRajeswari <shanmukha.725374@gmail.com>
2017-07-14 10:17:07 +0530

    Fbuild new version

---
+++ .build/debug/Ccmark.build/cmark_ctype.c.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /home/iiit/Desktop/programs/devika/.build/checkouts/Kitura-Markdown--1889999287584208261/Sources/Ccmark/cmark_ctype.c \
+  /home/iiit/Desktop/programs/devika/.build/checkouts/Kitura-Markdown--1889999287584208261/Sources/Ccmark/include/module.modulemap \
+  /usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include/module.modulemap \
+  /usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include/stdint.h \
+  /usr/include/stdint.h /usr/include/features.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /home/iiit/Desktop/programs/devika/.build/checkouts/Kitura-Markdown--1889999287584208261/Sources/Ccmark/cmark_ctype.h
